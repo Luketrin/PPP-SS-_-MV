100f7ee92bfec9d10253ea4edcc94146645dfe8b
Luketrin <luketrinity23@gmail.com>
2024-06-07 16:28:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const abrirVideoButton = document.getElementById('abrir-video');
+
+abrirVideoButton.addEventListener('click', () => {
+  window.open('https://youtu.be/cF1Na4AIecM?si=oyV4PEirFTUEUIOl'); 
+});
